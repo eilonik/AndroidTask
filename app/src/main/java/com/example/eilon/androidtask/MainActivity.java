508dfedef0e229cede97095220b6722bad20505c
eilonik <eilnoik@gmail.com>
2016-02-07 19:01:28 +0200

    adding  test activity

--- app/src/main/java/com/example/eilon/androidtask/MainActivity.java
+++ app/src/main/java/com/example/eilon/androidtask/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.eilon.androidtask;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,9 +25,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View v = new com.example.eilon.taskstripview.TaskView(this);
-        setContentView(v);
-        /*
+//        View v = new com.example.eilon.taskstripview.TaskView(this);
+//        setContentView(v);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -36,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
+        });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();

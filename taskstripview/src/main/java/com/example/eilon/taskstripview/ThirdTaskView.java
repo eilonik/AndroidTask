62ff468eb4563feabd318c8412d64227224f7073
eilonik <eilnoik@gmail.com>
2016-02-10 17:43:58 +0200

    task 3 twitter share fixed

--- taskstripview/src/main/java/com/example/eilon/taskstripview/ThirdTaskView.java
+++ taskstripview/src/main/java/com/example/eilon/taskstripview/ThirdTaskView.java
@@ -6,8 +6,10 @@ package com.example.eilon.taskstripview;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -46,10 +48,14 @@ public class ThirdTaskView extends TaskView {
 
 
                 // Sharing on Facebook
-                Intent facebookStatus = new Intent(Intent.ACTION_VIEW);
-                facebookStatus.setData(Uri.parse("http://m.facebook.com/sharer.php?u="
-                        + Uri.encode(message)));
-                parentTaskStripView.getContext().startActivity(facebookStatus);
+                if(isPackageInstalled("com.facebook.katana", parentTaskStripView.getContext())) {
+                    //TODO
+                }
+
+                else {
+                    shareStatusOnWeb("http://m.facebook.com/sharer.php?u=" + Uri.encode(message));
+                }
+
                 break;
 
             case STATE_2:
@@ -57,9 +63,17 @@ public class ThirdTaskView extends TaskView {
                 textView.setText("Send intent");
 
                 // Sharing on tweeter
-                Intent tweet = new Intent(Intent.ACTION_VIEW);
-                tweet.setData(Uri.parse("http://twitter.com/?status=" + Uri.encode(message)));
-                parentTaskStripView.getContext().startActivity(tweet);
+                if(isPackageInstalled("com.twitter.android", parentTaskStripView.getContext()))
+                {
+                    shareStatusOnApp("com.twitter.android",
+                            "com.twitter.android.composer.ComposerActivity", message);
+
+                }
+                else
+                {
+                    shareStatusOnWeb("https://twitter.com/intent/tweet?text=" + message);
+                }
+
                 break;
 
             case STATE_3:
@@ -99,4 +113,46 @@ public class ThirdTaskView extends TaskView {
 
         }
     }
+
+
+    // This method checks if a package is installed on the device
+    protected boolean isPackageInstalled(String packageName, Context context) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            Log.e("TWITTER","INSTALLED");
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("TWITTER","NOT INSTALLED");
+            return false;
+        }
+    }
+
+    // This method shares a status on a specified app
+    protected void shareStatusOnApp(String packageName, String className, String message) {
+        Intent status;
+
+        if(className == "") {
+            status = new Intent(Intent.ACTION_SEND);
+            status.setPackage(packageName);
+            status.putExtra(Intent.EXTRA_SUBJECT, message);
+            status.setType("text/plain");
+        }
+        else {
+            status = new Intent(Intent.ACTION_VIEW);
+            status.setClassName(packageName, className);
+            status.setType("text/*");
+        }
+        Log.e("MSG", message);
+        status.putExtra(android.content.Intent.EXTRA_TEXT, message);
+        parentTaskStripView.getContext().startActivities(new Intent[]{status});
+    }
+
+    // This method shares a status through a link in case the target app is not installed
+    protected void shareStatusOnWeb(String shareUrl) {
+        Uri uri = Uri.parse(shareUrl);
+        Intent status = new Intent(Intent.ACTION_VIEW, uri);
+        parentTaskStripView.getContext().startActivities(new Intent[]{status});
+    }
+
 }
